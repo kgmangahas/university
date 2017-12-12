8103773c68999cbad0c38d012dee66336d535aa5
Kate <kateline.mangahas@gmail.com>
2017-12-12 22:47:39 +0800

    Added search function.

--- wp-content/themes/fictional-university/footer.php
+++ wp-content/themes/fictional-university/footer.php
@@ -50,6 +50,7 @@
 
 </div>
 </footer>
+  
 
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/fictional-university/js/scripts-bundled.js
+++ wp-content/themes/fictional-university/js/scripts-bundled.js
@@ -10547,13 +10547,123 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Search = function Search() {
-	_classCallCheck(this, Search);
+var Search = function () {
+	//describe and initiate object
+	function Search() {
+		_classCallCheck(this, Search);
 
-	alert("hello, I am a search");
-};
+		this.addSearchHTML();
+		this.resultsDiv = (0, _jquery2.default)("#search-overlay__results");
+		this.openButton = (0, _jquery2.default)(".js-search-trigger");
+		this.closeButton = (0, _jquery2.default)(".search-overlay__close");
+		this.searchOverlay = (0, _jquery2.default)(".search-overlay");
+		this.searchField = (0, _jquery2.default)("#search-term");
+		this.events();
+		this.isOverlayOpen = false;
+		this.isSpinnerVisible = false;
+		this.prevValue;
+		this.typingTimer;
+	}
+
+	//events
+
+
+	_createClass(Search, [{
+		key: "events",
+		value: function events() {
+			this.openButton.on("click", this.openOverlay.bind(this));
+			this.closeButton.on("click", this.closeOverlay.bind(this));
+			(0, _jquery2.default)(document).on("keydown", this.keyPressDispatcher.bind(this));
+			this.searchField.on("keyup", this.typingLogic.bind(this));
+		}
+
+		//methods
+
+	}, {
+		key: "typingLogic",
+		value: function typingLogic() {
+			if (this.searchField.val() != this.prevValue) {
+				clearTimeout(this.typingTimer);
+				if (this.searchField.val()) {
+					if (!this.isSpinnerVisible) {
+						this.resultsDiv.html('<div class="spinner-loader"></div>');
+						this.isSpinnerVisible = true;
+					}
+					this.typingTimer = setTimeout(this.getResults.bind(this), 750);
+				} else {
+					this.resultsDiv.html('');
+					this.isSpinnerVisible = false;
+				}
+			}
+			this.prevValue = this.searchField.val();
+		}
+	}, {
+		key: "getResults",
+		value: function getResults() {
+			var _this = this;
+
+			_jquery2.default.when(_jquery2.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()), _jquery2.default.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())).then(function (posts, pages) {
+
+				var combinedResults = posts[0].concat(pages[0]);
+				_this.resultsDiv.html("\n\t\t\t\t<h2 class=\"search-overlay__section-title\">General Information</h2>\n\t\t\t\t" + (combinedResults.length ? '<ul class="link-list min-list">' : '<p>No General Information</p>') + "\t\t\t\t\n\t\t\t\t\t" + combinedResults.map(function (item) {
+					return "<li><a href=\"" + item.link + "\">" + item.title.rendered + "</a></li>";
+				}).join('') + "\n\t\t\t\t\t\n\t\t\t\t" + (combinedResults.length ? '</ul>' : '') + "\n\t\t\t\t");
+				_this.isSpinnerVisible = false;
+			}, function () {
+				_this.resultsDiv.html('<p>Unexpected error: please try again.</p>');
+			});
+		}
+	}, {
+		key: "keyPressDispatcher",
+		value: function keyPressDispatcher(e) {
+
+			if (e.keyCode == 83 && !this.isOverlayOpen && !(0, _jquery2.default)("input, textarea").is(':focus')) {
+				this.openOverlay();
+			}
+
+			if (e.keyCode == 27 && this.isOverlayOpen) {
+				this.closeOverlay();
+			}
+		}
+	}, {
+		key: "openOverlay",
+		value: function openOverlay() {
+			var _this2 = this;
+
+			this.searchOverlay.addClass("search-overlay--active");
+			(0, _jquery2.default)("body").addClass("body-no-scroll");
+			this.searchField.val('');
+			setTimeout(function () {
+				return _this2.searchField.focus();
+			}, 301);
+			this.isOverlayOpen = true;
+		}
+	}, {
+		key: "closeOverlay",
+		value: function closeOverlay() {
+			this.searchOverlay.removeClass("search-overlay--active");
+			(0, _jquery2.default)("body").removeClass("body-no-scroll");
+			this.isOverlayOpen = false;
+		}
+	}, {
+		key: "addSearchHTML",
+		value: function addSearchHTML() {
+			(0, _jquery2.default)("body").append("\n\t\t\t<div class=\"search-overlay\">\n\t\t\t  <div class=\"search-overlay__top\">\n\t\t\t    <div class=\"container\">\n\t\t\t      <i class=\"fa fa-search search-overlay__icon\" aria-hidden=\"true\"></i> \n\t\t\t      <input type=\"text\" class=\"search-term\" placeholder=\"What are you looking for?\" id=\"search-term\">\n\t\t\t       <i class=\"fa fa-window-close search-overlay__close\" aria-hidden=\"true\"></i> \n\t\t\t    </div>\n\t\t\t    \n\t\t\t  </div>\n\n\t\t\t  <div class=\"container\">\n\t\t\t    <div id=\"search-overlay__results\">\n\t\t\t      \n\t\t\t    </div>\n\t\t\t  </div>\n\t\t\t</div>\n\n\t\t\t");
+		}
+	}]);
+
+	return Search;
+}();
 
 exports.default = Search;
 
